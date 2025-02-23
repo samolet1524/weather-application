6d1b2083ee913ead7345265a8a7d4496619eb231
Svetlana Burikova <svetlana.burikova@mvise.de>
2025-02-23 20:34:02 +0100

    weather-service

---
+++ weather-service/src/main/java/ru/otus/weatherservice/controller/WeatherServiceController.java
@@ -0,0 +1,9 @@
+package ru.otus.weatherservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("weather/service")
+public class WeatherServiceController {
+}
---
+++ weather-service/src/main/java/ru/otus/weatherservice/service/WeatherService.java
@@ -0,0 +1,12 @@
+package ru.otus.weatherservice.service;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class WeatherService {
+
+
+}
