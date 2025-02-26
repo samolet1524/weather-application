87c234627023e6c319599c5cf7a306ad96dc01f2
Svetlana Burikova <svetlana.burikova@mvise.de>
2025-02-26 21:42:07 +0100

    archive-service: helm not working

---
+++ archive-service/src/main/java/ru/otus/archiveservice/util/CommonConstants.java
@@ -0,0 +1,9 @@
+package ru.otus.archiveservice.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CommonConstants {
+
+    public static final String ARTIST = "Art";
+}
