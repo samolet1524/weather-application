90bbed15e175755a9bedbbe0ea46ceb5b7cdd242
Svetlana Burikova <svetlana.burikova@mvise.de>
2025-02-27 14:16:09 +0100

    archive-service: not works

--- archive-service/src/main/java/ru/otus/archiveservice/scheduled/ArchiveJob.java
+++ archive-service/src/main/java/ru/otus/archiveservice/scheduled/ArchiveJob.java
@@ -4,8 +4,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,10 +12,7 @@ import ru.otus.archiveservice.service.ArchiveService;
 import ru.otus.model.astronomy.AstronomyResponse;
 import ru.otus.model.weather.RealTimeResponse;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.URI;
 
 /**
  * {@code ArchiveJob} contains periodic tasks
@@ -31,7 +26,7 @@ public class ArchiveJob {
     final ArchiveService archiveService;
     final WebClient webClient;
 
-    private String ipAddress;
+    private final String IP_ADDRESS = "212.202.11.111";
 
     /**
      * Calls the {@code weather-service} to get current weather information for the application host location
@@ -40,10 +35,14 @@ public class ArchiveJob {
      */
     @Scheduled(fixedDelay = 300000)
     public void runGetWeatherPointJob() {
-        log.info("Start job to getting weather point");
+        log.info("Start job to getting weather point for {}", IP_ADDRESS);
         webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/real/{ip}")
-                        .build(ipAddress))
+                .uri(uriBuilder -> {
+                    URI build = uriBuilder.path("/real/{ip}")
+                            .build(IP_ADDRESS);
+                    log.info("job task: uri to send {}", build);
+                    return build;
+                })
                 .retrieve()
                 .bodyToMono(RealTimeResponse.class)
                 .flatMap(Mono::just)
@@ -57,25 +56,15 @@ public class ArchiveJob {
      * with a fixed period between the end of the last invocation and the start of the next. Received information
      * will be saved in the database.
      */
-    //    @Scheduled(cron = "0 0 1 * * ?")
-    @Scheduled(cron = "0 03 21  * * ?")
+    @Scheduled(cron = "0 0 1 * * ?")
     public void runGetAstronomyPointJob() {
         webClient.get()
                 .uri(uriBuilder -> uriBuilder.path("/astronomy/{ip}")
-                        .build(ipAddress))
+                        .build(IP_ADDRESS))
                 .retrieve()
                 .bodyToMono(AstronomyResponse.class)
                 .flatMap(Mono::just)
                 .subscribe(archiveService::addAstronomyPointToArchive);
     }
 
-    @EventListener
-    private void onApplicationEvent(ContextRefreshedEvent event) {
-        try (final DatagramSocket datagramSocket = new DatagramSocket()) {
-            datagramSocket.connect(InetAddress.getByName("8.8.8.8"), 12345);
-            ipAddress = "212.202.11.111";
-        } catch (UnknownHostException | SocketException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
