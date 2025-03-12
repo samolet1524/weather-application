fcf75838909d97b866236bcd68c0b215fb4eeab7
Svetlana Burikova <svetlana.burikova@mvise.de>
2025-03-12 10:41:28 +0100

    HELP.md

--- archive-service/src/main/java/ru/otus/archiveservice/service/ArchiveService.java
+++ archive-service/src/main/java/ru/otus/archiveservice/service/ArchiveService.java
@@ -45,9 +45,9 @@ public class ArchiveService {
                 .ifPresentOrElse(weatherPoint::setLocation,
                         () -> weatherPoint.setLocation(locationRepository.save(locationMapper.toLocation(response.getLocation()))));
         weatherPointRepository.save(weatherPoint);
-        metricsService.registerWeatherValueTemperature("temp_c", weatherPoint.getTempC());
-        metricsService.registerWeatherValueTemperature("feelslike_c", weatherPoint.getFeelsLikeC());
-        metricsService.registerWeatherValueHumidity(Double.valueOf(weatherPoint.getHumidity()));
+        metricsService.getTempGauge().set(weatherPoint.getTempC());
+        metricsService.getHumidityGauge().set(weatherPoint.getHumidity());
+        metricsService.getFeelsLikeGauge().set(weatherPoint.getFeelsLikeC());
     }
 
     /**
@@ -61,6 +61,6 @@ public class ArchiveService {
                 .ifPresentOrElse(astronomy::setLocation,
                         () -> astronomy.setLocation(locationRepository.save(locationMapper.toLocation(response.getLocation()))));
         astronomyRepository.save(astronomy);
-        metricsService.registerAstronomy(Duration.between(astronomy.getSunrise().toLocalTime(), astronomy.getSunset().toLocalTime()).toMinutes());
+        metricsService.getDayLengthGauge().set(Duration.between(astronomy.getSunrise().toLocalTime(), astronomy.getSunset().toLocalTime()).toMinutes());
     }
 }
--- archive-service/src/main/java/ru/otus/archiveservice/service/CustomMetricsService.java
+++ archive-service/src/main/java/ru/otus/archiveservice/service/CustomMetricsService.java
@@ -1,9 +1,10 @@
 package ru.otus.archiveservice.service;
 
 import com.google.common.util.concurrent.AtomicDouble;
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
@@ -14,28 +15,26 @@ import java.util.concurrent.atomic.AtomicLong;
  * {@code CustomMetricsService} stores custom metrics
  */
 @Service
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @RequiredArgsConstructor
 public class CustomMetricsService {
 
-    MeterRegistry meterRegistry;
+    final MeterRegistry meterRegistry;
 
-    public void registerWeatherValueTemperature(String tag, Double value) {
-        AtomicDouble atomicDouble = new AtomicDouble(value);
-        Gauge.builder("temperature.point", () -> atomicDouble)
-                .tags("temperature", tag)
-                .register(meterRegistry);
-    }
+    @Getter
+    AtomicDouble tempGauge;
+    @Getter
+    AtomicDouble feelsLikeGauge;
+    @Getter
+    AtomicDouble humidityGauge;
+    @Getter
+    AtomicLong dayLengthGauge;
 
-    public void registerWeatherValueHumidity(Double value) {
-        AtomicDouble atomicDouble = new AtomicDouble(value);
-        Gauge.builder("humidity.point", () -> atomicDouble)
-                .register(meterRegistry);
-    }
-
-    public void registerAstronomy(Long value) {
-        AtomicLong atomicLong = new AtomicLong(value);
-        Gauge.builder("day.length",() -> atomicLong)
-                .register(meterRegistry);
+    @PostConstruct
+    public void init() {
+        dayLengthGauge = meterRegistry.gauge("day.length", new AtomicLong(0));
+        tempGauge = meterRegistry.gauge("temperature.point", new AtomicDouble(0));
+        feelsLikeGauge = meterRegistry.gauge("temperature.point.feelsLike", new AtomicDouble(0));
+        humidityGauge = meterRegistry.gauge("humidity.point", new AtomicDouble(0));
     }
 }
